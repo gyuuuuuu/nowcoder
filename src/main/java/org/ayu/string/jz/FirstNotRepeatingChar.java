35c157b0e927115ebc3d24906d188848efe7d794
WeipingGan <gan.weiping@thoughtworks.com>
2022-09-18 00:19:00 +0800

    [List] Implement first not repeating char.

---
+++ src/main/java/org/ayu/string/jz/FirstNotRepeatingChar.java
@@ -0,0 +1,28 @@
+package org.ayu.string.jz;
+
+
+import java.util.HashMap;
+
+public class FirstNotRepeatingChar {
+    public int firstNotRepeatingChar(String str) {
+        if (str.length() == 0) return -1;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            String k = str.substring(i, i + 1);
+            int times = map.getOrDefault(k, 0);
+            map.put(k, ++times);
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (map.get(str.substring(i, i + 1)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String hello = "hellohello!";
+        System.out.println(new FirstNotRepeatingChar().firstNotRepeatingChar(hello));
+    }
+}
